be99fe6c716f752bf0270c2c8bb06196963bff42
Li Qiang <liq3ea@gmail.com>
2016-11-23 13:53:34 +0100

    9pfs: adjust the order of resource cleanup in device unrealize
    Unrealize should undo things that were set during realize in
    reverse order. So should do in the error path in realize.
    
    Signed-off-by: Li Qiang <liq3ea@gmail.com>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Signed-off-by: Greg Kurz <groug@kaod.org>
    (cherry picked from commit 4774718e5c194026ba5ee7a28d9be49be3080e42)

--- hw/9pfs/9p.c
+++ hw/9pfs/9p.c
@@ -3481,8 +3481,8 @@ int v9fs_device_realize_common(V9fsState *s, Error **errp)
     rc = 0;
 out:
     if (rc) {
-        g_free(s->ctx.fs_root);
         g_free(s->tag);
+        g_free(s->ctx.fs_root);
         v9fs_path_free(&path);
     }
     return rc;
@@ -3490,8 +3490,8 @@ out:
 
 void v9fs_device_unrealize_common(V9fsState *s, Error **errp)
 {
-    g_free(s->ctx.fs_root);
     g_free(s->tag);
+    g_free(s->ctx.fs_root);
 }
 
 static void __attribute__((__constructor__)) v9fs_set_fd_limit(void)
